415eedd024b77d10a95cdfeaa2e1a041c83ee009
Сергей Каращук <kspost01@gmail.com>
2022-12-13 23:10:00 +0200

    1
    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,25 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+console.log(createGalleryCardsMarkup(galleryItems));
+
+function createGalleryCardsMarkup(galleryItems)  {
+    const markup = galleryItems.map(({ preview, original, description }) => {
+        `
+        <div class="gallery__item">
+       <a class="gallery__link" href="${original}">
+         <img
+           class="gallery__image"
+           src="${preview}"
+           data-source="${original}"
+           alt="${description}"
+         />
+       </a>
+     </div>
+        `; 
+    });
+    console.log(markup);
+  }
+
