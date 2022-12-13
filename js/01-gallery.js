e612c67d5a7e1b72d577da833032d931fab65dd0
Сергей Каращук <kspost01@gmail.com>
2022-12-14 01:52:45 +0200

    3
    3

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,13 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const cardsMarkup = createGalleryCardsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+galleryContainer.addEventListener('click', onGalleryContainerClick)
+
+function onGalleryContainerClick(evt) {
+   const isGalleryImage = evt.target.classList.contains('gallery__image');
+    if (!isGalleryImage) {
+        return
+     }
+     console.log(evt.target);
+}
 
 console.log(createGalleryCardsMarkup(galleryItems));
 
 function createGalleryCardsMarkup(galleryItems)  {
     return galleryItems.map(({ preview, original, description }) => {
-        `
+       return `
         <div class="gallery__item">
        <a class="gallery__link" href="${original}">
          <img
@@ -19,6 +35,6 @@ function createGalleryCardsMarkup(galleryItems)  {
        </a>
      </div>
         `; 
-    });
+    }).join('');
   }
 
