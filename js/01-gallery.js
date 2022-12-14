aa0b652a7a2df00e5ea05089b13a40b5a703899c
Сергей Каращук <kspost01@gmail.com>
2022-12-14 17:30:30 +0200

    4
    4

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,25 +2,51 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 
-console.log(galleryItems);
+// console.log(galleryItems);
 
+// звертаюсь до галереї що є масивом об'єктів
 const galleryContainer = document.querySelector('.gallery');
+
+// створюю змінну що є строками карток масиву
 const cardsMarkup = createGalleryCardsMarkup(galleryItems);
 
+// створюю контейнер і додаю в нього всі картки галереї що є строками
 galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
 
+// додаю слухача подій на КЛІК
 galleryContainer.addEventListener('click', onGalleryContainerClick)
 
+// створюю функцію що буде перевіряти КЛІК та за допомогою basicLightbox - відкривати модалку
+//  з обраною картинкою. Та закривати при кліку на Escape
 function onGalleryContainerClick(evt) {
+    blockStandartAction(evt);
+
+
    const isGalleryImage = evt.target.classList.contains('gallery__image');
     if (!isGalleryImage) {
         return
      }
      console.log(evt.target);
+
+     const instance = basicLightbox.create(`<img src=${evt.target.dataset.source} width="800" height="600">`);
+ instance.show();
+
+ galleryContainer = addEventListener("keydown", (evt) =>{
+    if (evt.code === "Escape" ) {
+        instance.close();
+    }
+ });
 }
 
-console.log(createGalleryCardsMarkup(galleryItems));
+// console.log(createGalleryCardsMarkup(galleryItems));
 
+// функція заборони стандартних дій щоб браузер не відкривав картинку у іншому вікні
+function blockStandartAction(evt) {
+    evt.preventDefault();
+}
+
+
+// Створюю розмітку для картки як шаблон
 function createGalleryCardsMarkup(galleryItems)  {
     return galleryItems.map(({ preview, original, description }) => {
        return `
@@ -37,4 +63,8 @@ function createGalleryCardsMarkup(galleryItems)  {
         `; 
     }).join('');
   }
+
+  
+
+
 
